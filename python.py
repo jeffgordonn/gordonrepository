c1c4452cf3717e6b649a613902b8bab4f4befdab
jeffgordonn <122314915+jeffgordonn@users.noreply.github.com>
2023-04-11 15:11:58 -0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,7 @@
+print("Welcome to the Gordon Repository for the IBM Data Science Professional Certificate")
+
+guest_name= input("What is your name? ")
+
+print("Hello {}! Nice to have you!").format(guest_name)
+
+print("We will have more updates shortly")
